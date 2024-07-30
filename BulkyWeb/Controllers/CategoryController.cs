905f61b24163df6e06a99e0eac12671ebada4b98
Kasun <kasun.sdkh@gmail.com>
2024-07-30 17:20:19 +0530

    category

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -32,6 +32,70 @@ namespace BulkyWeb.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                // Adding custom validation message
+                ModelState.AddModelError("Name", "The display order cannot extractly match the name");
+            }
+            if (obj.Name.ToLower() == "test")
+            {
+                // Even no validation Field this shows under the validation summary 
+                ModelState.AddModelError("", "Test is an invalid value");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+            Category categoryFromDb = _db.Categories.Find(id);
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+            if (categoryFromDb == null){
+                 
+                 return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                // Adding custom validation message
+                ModelState.AddModelError("Name", "The display order cannot extractly match the name");
+            }
+            if (obj.Name.ToLower() == "test")
+            {
+                // Even no validation Field this shows under the validation summary 
+                ModelState.AddModelError("", "Test is an invalid value");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,7 +13,12 @@ namespace BulkyWeb.Models
         public int Id { get; set; }
 
         [Required] 
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
         public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage ="Display Order must be  between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
